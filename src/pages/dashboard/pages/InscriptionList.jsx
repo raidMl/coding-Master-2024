e332fa25c915e14835a0cd4eb1645d6005f52ff8
raidMl <raidreus.22@gmail.com>
2024-03-06 12:30:20 +0100

    edit inscription

--- src/pages/dashboard/pages/InscriptionList.jsx
+++ src/pages/dashboard/pages/InscriptionList.jsx
@@ -1,12 +1,38 @@
-import React from 'react';
+import React ,{useEffect} from 'react';
 import { Container, Grid, Paper } from '@mui/material';
 import Chart from '../components/Chart';
 import Deposits from '../components/Deposits';
 import SideBare from '../components/sideBare';
 import Box from '@mui/material/Box';
 import CssBaseline from '@mui/material/CssBaseline';
+import { useSelector } from 'react-redux';
+import axios from 'axios';
 
 const inscriptionList = () => {
+    const auth = useSelector(state => state.auth);
+
+    useEffect(() => {
+      const fetchData = async () => {
+        try {
+          const token = auth.token;
+          const id = auth.uuid;
+  
+          const config = {
+            headers: {
+              Authorization: token,
+            },
+          };
+  
+          const response = await axios.get(`https://progres.mesrs.dz/api/infos/bac/${id}/notes`, config);
+          console.log(response.data);
+        } catch (error) {
+          console.error('Error fetching data:', error);
+        }
+      };
+  
+      fetchData();
+    }, [auth]);
+  
   return (
     <div style={{ height: 'max-content', width: '100%', overflow: 'auto' }}>
       <Box sx={{ display: 'flex' }}>
